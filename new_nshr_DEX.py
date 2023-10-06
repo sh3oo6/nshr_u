45d5d9a3a23d1362673011964abf4c22819278f3
sh3oo6 <141688473+sh3oo6@users.noreply.github.com>
2023-10-06 14:36:35 +0300

    Update new_nshr_DEX.py

--- new_nshr_DEX.py
+++ new_nshr_DEX.py
@@ -3,7 +3,7 @@ import asyncio
 import re, requests
 
 try:
-    client = open('/root/nshr_u/prift.txt', 'r').read().replace('\n', '')
+    client = open('/root/plus/prift.txt', 'r').read().replace('\n', '')
     print(client)
     client = f'/root/session/{client}'
     DEX = TelegramClient(client, 22160733, 'c95e81b40eba3404ac130f4a9f235e4c')
