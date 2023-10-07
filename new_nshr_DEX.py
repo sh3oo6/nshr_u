9899867b58dc7542ea38dd3a305070e7052a1f82
sh3oo6 <141688473+sh3oo6@users.noreply.github.com>
2023-10-07 16:37:08 +0300

    Update new_nshr_DEX.py

--- new_nshr_DEX.py
+++ new_nshr_DEX.py
@@ -115,7 +115,7 @@ Dexdex اهلاً وسهلاً
 لفحص يوزر ما اذا كان مرفوع مزاد او لا او مرفوع ومباع اكتب DexUser + يوزرك ، مثال
 User @LuLuu
 اذا واجهت مشاكل راسلني 
-Owner : @LuLuu ,  Channel : @iiiNil GGGGGG''')
+Owner : @LuLuu ,  Channel : @iiiNil GGGGGGff''')
 
 
 DEX.run_until_disconnected()
